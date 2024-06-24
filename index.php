899bfa3bc53b1ea04ed34cfcc45cd76cc13c05cc
AlanDAmario <vega93@live.it>
2024-06-24 19:43:24 +0200

    [upd] add Exeption with 2 catch

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 
 class Movie{
     //PROPRIETà
-    public $name;
-    public $type;
-    public $release;
-    private $boxOffice;
+    public string $name;
+    public string $type;
+    public int $release;
+    private  $boxOffice;
 
     //COSTRUTTORE, ALL INTERNO DELLE (PROPRIETà DA ANNUNCIARE)
     function __construct($name, $type, $release){
@@ -16,7 +16,7 @@ class Movie{
     }
 
     //GETTERS & SETTERS
-    function set_name($name){
+    function set_name(string $name){
         $this-> name = $name;
     }
 
@@ -24,14 +24,14 @@ class Movie{
         return $this-> name;
     }
 
-    function set_type($type){
+    function set_type(string $type){
         $this-> type = $type;
     }
     function get_type(){
         return $this -> type;
     }
 
-    function set_release($release){
+    function set_release(int $release){
         $this -> release = $release;
     }
 
@@ -39,29 +39,47 @@ class Movie{
          return $this -> release;
     }
 
-    function set_boxOffice($boxOffice){
-        $this -> boxOffice = $boxOffice;
-    }
 
-    function get_boxOffice(){
-        return $this -> boxOffice;
-    }
+     function set_boxOffice(int $boxOffice){
+        if(!is_int($boxOffice) || $boxOffice < 0 ){
+            throw new Exception("L'incasso deve essere un numero intero e maggiore di almeno 10 mln");
+        }
+         $this -> boxOffice = $boxOffice;
+     }
+
+     function get_boxOffice(){
+         return $this -> boxOffice;
+     }
+
+
 
     //METODI 
     function print(){
-        echo "Questo è il film <h3> $this->name , </h3> di tipo <h4> $this->type, </h4>  uscito il <h4> $this->release,</h4> l ' incasso è <h4>$this->boxOffice </h4> <br> <br>";
+        echo "Questo è il film <h3> $this->name , </h3> di tipo <h4> $this->type, </h4>  uscito il <h4> $this->release,</h4> l ' incasso è <h4>$this->boxOffice  mln</h4> <br> <br>";
     }
+
+    function getMessage(){
+        'Ops! Qualcosa è andato storto';
+    }
+
 }
 
-//ISTANZA DELL OGGETTO MOVIE
-//TRAMITE IL COSTRUTTORE ABBIMAO PASSATO DEI VALORI ALLE SUE PROPRIETà
-$movie1 = new Movie('Kimetsu no yaiba', 'Anime/Action', '2020');
-$movie1 -> set_boxOffice(120 . 'mln');
-$movie1-> print();
+try {
+    //ISTANZA DELL OGGETTO MOVIE
+    //TRAMITE IL COSTRUTTORE ABBIMAO PASSATO DEI VALORI ALLE SUE PROPRIETà
+    $movie1 = new Movie('Kimetsu no yaiba', 'Anime/Action', '2020');
+    $movie1 -> set_boxOffice(120);
+    $movie1-> print();
+    
+    $movie2 = new Movie('Titanic', 'Drama', '1997');
+    $movie2 -> set_boxOffice(886);
+    $movie2 -> print();
 
-$movie2 = new Movie('Titanic', 'Drama', '1997');
-$movie2 -> set_boxOffice(886 . 'mln');
-$movie2 -> print();
+} catch (Exception $e) {
+    echo $e->getMessage();
+} catch (Error $e) {
+    echo 'errore fatale chiama Gateano :D';
+}
 
 
 ?>
