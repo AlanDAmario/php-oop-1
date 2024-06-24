fa922032d150286871ab1ce951e5f433975d9f76
AlanDAmario <vega93@live.it>
2024-06-24 17:34:43 +0200

    [upd] add class, costruct

--- index.php
+++ index.php
@@ -1,4 +1,25 @@
 <?php 
 
-class 
+class Movie{
+    //PROPRIETà
+    public $name;
+    public $type;
+    public $release;
+    private $boxOffice;
+
+    //COSTRUTTORE, ALL INTERNO DELLE (PROPRIETà DA ANNUNCIARE)
+    function __construct($name, $type, $release){
+        //RIFERIMENTO DELLE PROPRIETà
+        $this->name = $name;
+        $this->type = $type;
+        $this->release = $release;
+    }
+}
+
+//ISTANZA DELL OGGETTO MOVIE
+//TRAMITE IL COSTRUTTORE ABBIMAO PASSATO DEI VALORI ALLE SUE PROPRIETà
+$movie1 = new Movie('Kimetsu no yaiba', 'Anime/Action', '2020');
+var_dump($movie1);
+
+
 ?>
